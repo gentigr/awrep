7201adfb9f95f60d9c5aa3700c2f6119a37195b9
Ihar Kukharchuk <Ihar_Kukharchuk@epam.com>
2022-03-19 17:25:08 -0400

    F0002: [CORE] add basic body/remarks split for report

--- lib/aviation_weather_report.dart
+++ lib/aviation_weather_report.dart
@@ -1,17 +1,38 @@
 class BodySection {
-  const BodySection(String body);
+  final String body;
+  const BodySection(this.body);
 }
 
 class RemarksSection {
-  const RemarksSection(String remarks);
+  final String remarks;
+  const RemarksSection(this.remarks);
+}
+
+enum ReportType {
+  metar,
+  speci,
+}
+
+class ReportParsingUtils {
+  static getBodySectionFromReport(String report) {
+    return report.split(" RMK ")[0].trim();
+  }
+
+  static getRemarksSectionFromReport(String report) {
+    return report.split(" RMK ")[1].trim();
+  }
 }
 
 class AviationWeatherReport {
   final BodySection _body;
   final RemarksSection _remarks;
+  final String _report;
+
   AviationWeatherReport(String report)
-      : _body = BodySection(report),
-        _remarks = RemarksSection(report);
+      : _body = BodySection(ReportParsingUtils.getBodySectionFromReport(report)),
+        _remarks =
+            RemarksSection(ReportParsingUtils.getRemarksSectionFromReport(report)),
+        _report = report;
 
   bool get isParsed {
     return false;
--- test/aviation_weather_report_test.dart
+++ test/aviation_weather_report_test.dart
@@ -2,11 +2,23 @@ import 'package:test/test.dart';
 import 'package:metar/aviation_weather_report.dart';
 
 void main() {
-  test('Counter value should be incremented', () {
+  test('Check if report was aviation weather parsed completely', () {
     final report = AviationWeatherReport(
         "KJFK 190351Z 18004KT 1/4SM R04R/2000V3000FT BR OVC002 08/08 A3002 "
-        "RMK AO2 SFC VIS 3/4 SLP164 T00830083");
+            "RMK AO2 SFC VIS 3/4 SLP164 T00830083");
 
     expect(report.isParsed, false);
+  });
+  parsingUtils();
+}
+
+void parsingUtils() {
+  test('Test separation for body and remarks', () {
+    final body = "KJFK 190351Z 18004KT 1/4SM R04R/2000V3000FT BR OVC002 08/08 A3002";
+    final remarks = "AO2 SFC VIS 3/4 SLP164 T00830083";
+    final report = "$body RMK $remarks";
+
+    expect(ReportParsingUtils.getBodySectionFromReport(report), body);
+    expect(ReportParsingUtils.getRemarksSectionFromReport(report), remarks);
   });
 }
