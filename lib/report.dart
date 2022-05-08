b9b44eb90b37c184003cc5bbfb440369bc61b714
Ihar Kukharchuk <Ihar_Kukharchuk@epam.com>
2022-05-08 17:08:41 -0400

    F0022: [CORE] Refactor Report class
    - remove ReportParser class
    - add documentation

--- lib/report.dart
+++ lib/report.dart
@@ -1,21 +1,59 @@
 import 'package:awrep/body/body.dart';
 import 'package:awrep/remarks/remarks.dart';
-import 'package:awrep/report_parser.dart';
 
-// The primary report class that holds Body and Remarks
+/// The primary report class that holds [body] and [remarks] sections of report.
+/// This implementation follows the following technique of writing Dart code:
+/// [AVOID storing what you can calculate.](https://dart.dev/guides/language/effective-dart/usage#avoid-storing-what-you-can-calculate)
+/// As a result it may have additional performance penalty in comparison to the
+/// libraries that parses provided METAR/SPECI report completely during object
+/// construction phase.
 class Report {
-  final Body _body;
-  final Remarks _remarks;
+  static const String _remarksToken = " RMK ";
+  final String _report;
 
-  Report(String report)
-      : _body = Body(ReportParser.getBody(report)),
-        _remarks = Remarks(ReportParser.getRemarks(report));
+  /// Initializes Report class with string representation of METAR/SPECI report.
+  Report(this._report);
 
+  /// Compares Report objects for logical equality.
+  @override
+  bool operator ==(Object other) {
+    return other is Report && this.hashCode == other.hashCode;
+  }
+
+  /// Provides hashCode of Report object based on internal content.
+  @override
+  int get hashCode => _report.hashCode;
+
+  /// Creates [Body] initialized with body part of the report.
+  Body get body {
+    var body = _report;
+    var index = _report.indexOf(_remarksToken);
+    if (index != -1) {
+      body = _report.substring(0, index);
+    }
+    return Body(body);
+  }
+
+  /// Creates [Remarks] initialized with remarks part of the report.
+  Remarks get remarks {
+    var remarks = '';
+    var index = _report.indexOf(_remarksToken);
+    if (index != -1) {
+      remarks = _report.substring(index + 1);
+    }
+    return Remarks(remarks);
+  }
+
+  /// Generates string representation of the report.
+  ///
+  /// This method intentionally re-creates string representation of METAR/SPECI
+  /// report for additional cross-check whether all report's fields were parsed
+  /// and recognized as expected.
   @override
   String toString() {
-    if (_remarks.toString().isEmpty) {
-      return _body.toString();
+    if (remarks.toString().isEmpty) {
+      return body.toString();
     }
-    return "$_body $_remarks";
+    return "$body $remarks";
   }
 }
--- test/report_test.dart
+++ test/report_test.dart
@@ -1,26 +1,92 @@
+import 'package:awrep/body/body.dart';
+import 'package:awrep/remarks/remarks.dart';
 import 'package:awrep/report.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('Report', () {
+    group('equalityOperator', () {
+      reportEqualityOperator();
+    });
+    group('hashCode', () {
+      reportHashCode();
+    });
+    group('body', () {
+      reportBody();
+    });
+    group('remarks', () {
+      reportRemarks();
+    });
     group('toString', () {
       reportToString();
     });
   });
 }
 
+void reportEqualityOperator() {
+  test('Test equality operator for non-equality', () {
+    final report1 = 'KJFK 190351Z 18004KT OVC020 08/08 A3002 RMK AO2 T00830083';
+    final report2 = 'KJFK 190351Z 18004KT OVC002 08/08 A3002 RMK AO2 T00830083';
+
+    expect(Report(report1) == Report(report2), false);
+  });
+  test('Test equality operator for equality', () {
+    final report1 = 'KJFK 190351Z 18004KT OVC002 08/08 A3002 RMK AO2 T00830083';
+    final report2 = 'KJFK 190351Z 18004KT OVC002 08/08 A3002 RMK AO2 T00830083';
+
+    expect(Report(report1) == Report(report2), true);
+  });
+}
+
+void reportHashCode() {
+  test('Test hash generation for non-equality', () {
+    final report1 = 'KJFK 190351Z 18004KT OVC001 08/08 A3002 RMK AO2 T00830083';
+    final report2 = 'KJFK 190351Z 18004KT OVC002 08/08 A3002 RMK AO2 T00830083';
+
+    expect(Report(report1).hashCode == Report(report2).hashCode, false);
+  });
+  test('Test hash generation for equality', () {
+    final report1 = 'KJFK 190351Z 18004KT OVC002 08/08 A3002 RMK AO2 T00830083';
+    final report2 = 'KJFK 190351Z 18004KT OVC002 08/08 A3002 RMK AO2 T00830083';
+
+    expect(Report(report1).hashCode == Report(report2).hashCode, true);
+  });
+}
+
+void reportBody() {
+  test('Test separation for body', () {
+    final body =
+        'KJFK 190351Z 18004KT 1/4SM R04R/2000V3000FT BR OVC002 08/08 A3002';
+    final remarks = 'RMK AO2 SFC VIS 3/4 SLP164 T00830083';
+    final report = '$body $remarks';
+
+    expect(Report(report).body, Body(body));
+  });
+}
+
+void reportRemarks() {
+  test('Test separation for remarks', () {
+    final body =
+        'KJFK 190351Z 18004KT 1/4SM R04R/2000V3000FT BR OVC002 08/08 A3002';
+    final remarks = 'RMK AO2 SFC VIS 3/4 SLP164 T00830083';
+    final report = '$body $remarks';
+
+    expect(Report(report).remarks, Remarks(remarks));
+  });
+}
+
 void reportToString() {
-  test('Check if aviation weather report was parsed completely; with rmk', () {
+  test('Check basic string output with remarks', () {
     String report =
-        "KJFK 190351Z 18004KT 1/4SM R04R/2000V3000FT BR OVC002 08/08 A3002 "
-        "RMK AO2 SFC VIS 3/4 SLP164 T00830083";
+        'KJFK 190351Z 18004KT 1/4SM R04R/2000V3000FT BR OVC002 08/08 A3002 '
+        'RMK AO2 SFC VIS 3/4 SLP164 T00830083';
 
     expect(Report(report).toString() == report, true);
   });
 
-  test('Check if aviation weather report was parsed completely: no rmk', () {
+  test('Check basic string output without remarks', () {
     String report =
-        "KJFK 190351Z 18004KT 1/4SM R04R/2000V3000FT BR OVC002 08/08 A3002";
+        'KJFK 190351Z 18004KT 1/4SM R04R/2000V3000FT BR OVC002 08/08 A3002';
 
     expect(Report(report).toString() == report, true);
   });
