c1eb43c99eec2a64699ec41e2eff548e5b767a55
Ihar Kukharchuk <Ihar_Kukharchuk@epam.com>
2022-05-09 06:22:57 -0400

    F0022: [CORE] Add ReportType parsing into Body

--- lib/body/body.dart
+++ lib/body/body.dart
@@ -1,8 +1,25 @@
-// Body section of METAR/SPECI report.
+import 'report_type.dart';
+
+/// [BodyException] is thrown when there is a report parsing issue.
+class BodyException implements Exception {
+  final String message;
+
+  const BodyException(this.message);
+
+  String errMsg() => this.message;
+}
+
+/// Body section of METAR/SPECI report.
 class Body {
   String _body;
 
   Body(this._body);
+
+  /// Returns type of report in [ReportType] format.
+  ReportType get reportType {
+    var regExp = RegExp('^(?<report_type>[^ ]{5} )?(.*)\$');
+    return stringAsReportType(_regexMatch(regExp, 'report_type'));
+  }
 
   @override
   bool operator ==(Object other) {
@@ -15,5 +32,21 @@ class Body {
   @override
   String toString() {
     return this._body;
+  }
+
+  void _checkRegexMatch(RegExp regExp) {
+    if (!regExp.hasMatch(_body)) {
+      throw BodyException(
+          'Failed to find RegEx `$regExp` in report body `$_body`');
+    }
+    if (regExp.allMatches(_body).length > 1) {
+      throw BodyException('Too many matches were found by RegEx `$regExp` '
+          'in report body `$_body`');
+    }
+  }
+
+  String? _regexMatch(RegExp regExp, String name) {
+    _checkRegexMatch(regExp);
+    return regExp.firstMatch(_body)!.namedGroup(name);
   }
 }
--- test/body/body_test.dart
+++ test/body/body_test.dart
@@ -1,8 +1,12 @@
 import 'package:awrep/body/body.dart';
+import 'package:awrep/body/report_type.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('Body', () {
+    group('reportType', () {
+      bodyReportType();
+    });
     group('equalityOperator', () {
       bodyEqualityOperator();
     });
@@ -12,6 +16,37 @@ void main() {
     group('toString', () {
       bodyToString();
     });
+  });
+}
+
+void bodyReportType() {
+  test('Test undefined report type exception is not caught', () {
+    final String body = 'UNDEF KJFK 190351Z 18004KT BR OVC002 08/08 A3002';
+    var msg = 'Unexpected report type value: `UNDEF `';
+    var err = 'Invalid argument (name): No enum value with that name: "undef"';
+
+    expect(
+        () => Body(body).reportType,
+        throwsA(predicate((e) =>
+            e is ReportTypeException && e.message == '$msg, error: `$err`')));
+  });
+
+  test('Test no report type body', () {
+    final body = 'KJFK 190351Z 18004KT 1/4SM BR OVC002 08/08 A3002';
+
+    expect(Body(body).reportType, ReportType.none);
+  });
+
+  test('Test metar report type body', () {
+    final body = 'METAR KJFK 190351Z 18004KT 1/4SM BR OVC002 08/08 A3002';
+
+    expect(Body(body).reportType, ReportType.metar);
+  });
+
+  test('Test speci report type speci', () {
+    final body = 'SPECI KJFK 190351Z 18004KT 1/4SM OVC002 08/08 A3002';
+
+    expect(Body(body).reportType, ReportType.speci);
   });
 }
 
@@ -47,8 +82,8 @@ void bodyHashCode() {
 
 void bodyToString() {
   test('Test basic string output format', () {
-    final body =
-        'SPECI KJFK 190351Z AUTO 18004KT 1/4SM R04R/2000V3000FT BR OVC002 08/08 A3002';
+    final body = 'SPECI KJFK 190351Z AUTO 18004KT 1/4SM R04R/2000V3000FT '
+        'BR OVC002 08/08 A3002';
 
     expect(Body(body).toString(), body);
   });
