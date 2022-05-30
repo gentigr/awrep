0a7c79bf91f094b1b427cfcf71d8724f8223c2c3
Ihar Kukharchuk <Ihar_Kukharchuk@epam.com>
2022-05-30 14:39:16 -0400

    F0025: [CORE] Refactor visibility functionality

--- lib/src/body/visibility.dart
+++ lib/src/body/visibility.dart
@@ -1,51 +1,47 @@
-import 'package:awrep/body/report_distance_modifier.dart';
 import 'package:fraction/fraction.dart';
 
-/// [ReportVisibilityException] is thrown when there is a parsing error occurred
-/// during usage of getters of [ReportVisibility] object.
-class ReportVisibilityException implements Exception {
-  final String message;
+import '../common/distance_qualifier.dart';
+import '../common/regexp_decorator.dart';
 
-  const ReportVisibilityException(this.message);
-
-  String errMsg() => this.message;
-}
-
-/// This class holds report visibility group description.
-class ReportVisibility {
+/// The class represents visibility group description within [Report].
+class Visibility {
   final String _visibility;
 
-  /// Constructor of [ReportVisibility] object, provided string should be in the
-  /// following VVVVVSM format to be parsed properly.
-  const ReportVisibility(this._visibility);
+  /// Constructs a [Visibility] from string representation.
+  ///
+  /// Provided string should be in VVVVVSM format to be parsed properly, where
+  /// VVVVV is a distance in fraction or whole.
+  /// Throws [FormatException] if the provided value is not by format.
+  Visibility(this._visibility) {
+    var regExp = RegExpDecorator('^([MP])?([0-9 \\/]{1,5})SM\$');
+    regExp.verifySingleMatch(_visibility, this.runtimeType.toString());
+  }
 
-  /// This is the reported visibility distance.
+  /// The reported visibility distance.
   double get distance {
-    var regExp = RegExp(
-        '^([mMpP])?(?<whole>[0-9]{1,5})? ?((?<fraction>[0-9]{1,5}\\/[0-9]{1,5}))?SM');
-    var whole = _regexMatch(regExp, 'whole');
-    var fraction = _regexMatch(regExp, 'fraction');
-    _checkRegexHasWholeOrFraction(regExp, whole, fraction);
+    var regExp = RegExpDecorator('^([MP]{1})?(?<whole>[0-9]{1,5})? ?'
+        '((?<fraction>[0-9]{1,5}\\/[0-9]{1,5}))?SM');
+    var whole = regExp.getMatchByNameOptional(_visibility, 'whole');
+    var fraction = regExp.getMatchByNameOptional(_visibility, 'fraction');
+
+    if (whole == null && fraction == null) {
+      throw FormatException('Either whole or fraction must be present in '
+          'visibility section, neither field was found by RegEx `$regExp` '
+          'in report visibility section `$_visibility`');
+    }
+
     return Fraction.fromString(whole ?? '0').toDouble() +
         Fraction.fromString(fraction ?? '0').toDouble();
   }
 
-  /// This is the reported visibility distance modifier.
+  /// The reported visibility distance qualifier.
   ///
-  /// It returns values such as `less than`, `greater than` or `none`.
-  ReportDistanceModifier get modifier {
-    var regExp = RegExp('^(?<modifier>[pPmM]{1})');
-    var modifier = _regexMatchOptional(regExp, 'modifier');
-    return stringAsReportDistanceModifier(modifier);
+  /// Returns values such as `less than`, `more than` or `none`.
+  DistanceQualifier get qualifier {
+    var regExp = RegExpDecorator('^(?<qualifier>[PM]{1})');
+    var qualifier = regExp.getMatchByNameOptional(_visibility, 'qualifier');
+    return DistanceQualifier(qualifier);
   }
-
-  @override
-  bool operator ==(Object other) {
-    return other is ReportVisibility && this.hashCode == other.hashCode;
-  }
-
-  @override
-  int get hashCode => _visibility.hashCode;
 
   @override
   String toString() {
@@ -54,59 +50,14 @@ class ReportVisibility {
     if (fraction.numerator == 0) {
       distanceStr = fraction.toFraction().toString();
     }
-    return '${modifier.string}${distanceStr}SM';
+    return '${qualifier.toString()}${distanceStr}SM';
   }
 
-  bool _checkRegexHasMatch(RegExp regExp, {bool noThrow = false}) {
-    if (regExp.hasMatch(_visibility)) {
-      return true;
-    }
-    if (noThrow) {
-      return false;
-    }
-    var err =
-        'Failed to find RegEx `$regExp` in report visibility section `$_visibility`';
-    print(err);
-    throw ReportVisibilityException(err);
+  @override
+  bool operator ==(Object other) {
+    return other is Visibility && this.hashCode == other.hashCode;
   }
 
-  void _checkRegexHasWholeOrFraction(
-      RegExp regExp, String? whole, String? fraction) {
-    if (whole != null || fraction != null) {
-      return;
-    }
-    var err = 'Either whole or fraction must be present in visibility section, '
-        'neither field was found by RegEx `$regExp` '
-        'in report visibility section `$_visibility`';
-    print(err);
-    throw ReportVisibilityException(err);
-  }
-
-  bool _checkRegexHasOnlyOneMatch(RegExp regExp, {bool noThrow = false}) {
-    if (regExp.allMatches(_visibility).length == 1) {
-      return true;
-    }
-    if (noThrow) {
-      return false;
-    }
-    var err = 'Too many matches were found by RegEx `$regExp` '
-        'in report visibility section `$_visibility`';
-    print(err);
-    throw ReportVisibilityException(err);
-  }
-
-  String? _regexMatch(RegExp regExp, String name) {
-    _checkRegexHasMatch(regExp);
-    _checkRegexHasOnlyOneMatch(regExp);
-    return regExp.firstMatch(_visibility.trim())!.namedGroup(name);
-  }
-
-  String? _regexMatchOptional(RegExp regExp, String name) {
-    bool hasMatch = _checkRegexHasMatch(regExp, noThrow: true);
-    if (!hasMatch) {
-      return null;
-    }
-    _checkRegexHasOnlyOneMatch(regExp);
-    return regExp.firstMatch(_visibility.trim())!.namedGroup(name);
-  }
+  @override
+  int get hashCode => _visibility.hashCode;
 }
--- test/src/body/visibility_test.dart
+++ test/src/body/visibility_test.dart
@@ -1,287 +1,296 @@
-import 'package:awrep/body/report_distance_modifier.dart';
-import 'package:awrep/body/report_visibility.dart';
-import 'package:awrep/body/report_wind.dart';
+import 'package:awrep/src/common/distance_qualifier.dart';
+import 'package:awrep/src/body/visibility.dart';
 import 'package:test/test.dart';
 
+import '../../test_utils.dart';
+
 void main() {
-  group('ReportVisibility', () {
+  group('Visibility', () {
+    group('constructor', () {
+      visibilityConstructor();
+    });
     group('distance', () {
-      reportVisibilityDistance();
+      visibilityDistance();
     });
-    group('modifier', () {
-      reportVisibilityModifier();
-    });
-    group('equalityOperator', () {
-      reportVisibilityEqualityOperator();
-    });
-    group('hashCode', () {
-      reportVisibilityHashCode();
+    group('qualifier', () {
+      visibilityQualifier();
     });
     group('toString', () {
-      reportVisibilityToString();
+      visibilityToString();
+    });
+    group('equalityOperator', () {
+      visibilityEqualityOperator();
+    });
+    group('hashCode', () {
+      visibilityHashCode();
     });
   });
 }
 
-void reportVisibilityDistance() {
-  test('Test no match', () {
-    var err = 'Failed to find RegEx `RegExp: pattern=^([mMpP])?'
-        '(?<whole>[0-9]{1,5})? ?((?<fraction>[0-9]{1,5}\\/[0-9]{1,5}))?SM '
-        'flags=` in report visibility section `S`';
+void visibilityConstructor() {
+  test('Test compliance with format, negative', () {
+    var err = 'Expected to find one match of `Visibility` format in `100000SM`,'
+        ' but found `0` using `RegExp: '
+        'pattern=^([MP])?([0-9 \\/]{1,5})SM\$ flags=`';
+    expectFormatException(() => Visibility('100000SM'), err);
+  });
 
-    expect(
-        () => ReportVisibility('S').distance,
-        throwsA(predicate(
-            (e) => e is ReportVisibilityException && e.message == err)));
+  test('Test compliance with format, negative with qualifier', () {
+    var err = 'Expected to find one match of `Visibility` format in `MM1000SM`,'
+        ' but found `0` using `RegExp: '
+        'pattern=^([MP])?([0-9 \\/]{1,5})SM\$ flags=`';
+    expectFormatException(() => Visibility('MM1000SM'), err);
+  });
+
+  test('Test compliance with format, positive', () {
+    expect(Visibility('M1000SM'), isA<Visibility>());
+  });
+}
+
+void visibilityDistance() {
+  test('Test no match', () {
+    var err = 'Either whole or fraction must be present in visibility section, '
+        'neither field was found by RegEx '
+        '`RegExp: pattern=^([MP]{1})?(?<whole>[0-9]{1,5})? ?'
+        '((?<fraction>[0-9]{1,5}\\/[0-9]{1,5}))?SM '
+        'flags=` in report visibility section `100 0SM`';
+
+    expect(() => Visibility('100 0SM').distance,
+        throwsA(predicate((e) => e is FormatException && e.message == err)));
   });
 
   test('Test at least whole or fraction is provided', () {
     var err = 'Either whole or fraction must be present in visibility section, '
-        'neither field was found by RegEx `RegExp: '
-        'pattern=^([mMpP])?(?<whole>[0-9]{1,5})? ?'
+        'neither field was found by RegEx '
+        '`RegExp: pattern=^([MP]{1})?(?<whole>[0-9]{1,5})? ?'
         '((?<fraction>[0-9]{1,5}\\/[0-9]{1,5}))?SM '
-        'flags=` in report visibility section `SM`';
+        'flags=` in report visibility section `1/1/1SM`';
 
-    expect(
-        () => ReportVisibility('SM').distance,
-        throwsA(predicate(
-            (e) => e is ReportVisibilityException && e.message == err)));
+    expect(() => Visibility('1/1/1SM').distance,
+        throwsA(predicate((e) => e is FormatException && e.message == err)));
   });
 
   test('Test less than grand visibility', () {
-    expect(ReportVisibility('M30000SM').distance, 30000);
+    expect(Visibility('M30000SM').distance, 30000);
   });
 
   test('Test grand visibility', () {
-    expect(ReportVisibility('30000SM').distance, 30000);
+    expect(Visibility('30000SM').distance, 30000);
   });
 
   test('Test greater than grand visibility', () {
-    expect(ReportVisibility('P30000SM').distance, 30000);
+    expect(Visibility('P30000SM').distance, 30000);
   });
 
   test('Test less than common visibility', () {
-    expect(ReportVisibility('M6SM').distance, 6);
+    expect(Visibility('M6SM').distance, 6);
   });
 
   test('Test common visibility', () {
-    expect(ReportVisibility('6SM').distance, 6);
+    expect(Visibility('6SM').distance, 6);
   });
 
   test('Test greater than common visibility', () {
-    expect(ReportVisibility('P6SM').distance, 6);
+    expect(Visibility('P6SM').distance, 6);
   });
 
   test('Test less than common fraction visibility', () {
-    expect(ReportVisibility('M1/2SM').distance, 1 / 2);
+    expect(Visibility('M1/2SM').distance, 1 / 2);
   });
 
   test('Test common fraction visibility', () {
-    expect(ReportVisibility('1/2SM').distance, 1 / 2);
+    expect(Visibility('1/2SM').distance, 1 / 2);
   });
 
   test('Test greater common fraction grand visibility', () {
-    expect(ReportVisibility('P1/2SM').distance, 1 / 2);
+    expect(Visibility('P1/2SM').distance, 1 / 2);
   });
 
   test('Test less than small fraction visibility', () {
-    expect(ReportVisibility('M1/16SM').distance, 1 / 16);
+    expect(Visibility('M1/16SM').distance, 1 / 16);
   });
 
   test('Test small fraction visibility', () {
-    expect(ReportVisibility('1/16SM').distance, 1 / 16);
+    expect(Visibility('1/16SM').distance, 1 / 16);
   });
 
   test('Test greater than small fraction visibility', () {
-    expect(ReportVisibility('P1/16SM').distance, 1 / 16);
+    expect(Visibility('P1/16SM').distance, 1 / 16);
   });
 
   test('Test less than mixed fraction visibility', () {
-    expect(ReportVisibility('M2 3/4SM').distance, 2 + 3 / 4);
+    expect(Visibility('M2 3/4SM').distance, 2 + 3 / 4);
   });
 
   test('Test mixed fraction visibility', () {
-    expect(ReportVisibility('2 3/4SM').distance, 2 + 3 / 4);
+    expect(Visibility('2 3/4SM').distance, 2 + 3 / 4);
   });
 
   test('Test greater than mixed fraction visibility', () {
-    expect(ReportVisibility('P2 3/4SM').distance, 2 + 3 / 4);
+    expect(Visibility('P2 3/4SM').distance, 2 + 3 / 4);
   });
 }
 
-void reportVisibilityModifier() {
-  test('Test no match', () {
-    expect(ReportVisibility('S').modifier, ReportDistanceModifier.none);
-  });
-
+void visibilityQualifier() {
   test('Test less than grand visibility', () {
-    expect(ReportVisibility('M30000SM').modifier, ReportDistanceModifier.less);
+    expect(Visibility('M30000SM').qualifier, DistanceQualifier.less);
   });
 
   test('Test grand visibility', () {
-    expect(ReportVisibility('30000SM').modifier, ReportDistanceModifier.none);
+    expect(Visibility('30000SM').qualifier, DistanceQualifier.none);
   });
 
   test('Test greater than grand visibility', () {
-    expect(
-        ReportVisibility('P30000SM').modifier, ReportDistanceModifier.greater);
+    expect(Visibility('P30000SM').qualifier, DistanceQualifier.more);
   });
 
   test('Test less than common visibility', () {
-    expect(ReportVisibility('M6SM').modifier, ReportDistanceModifier.less);
+    expect(Visibility('M6SM').qualifier, DistanceQualifier.less);
   });
 
   test('Test common visibility', () {
-    expect(ReportVisibility('6SM').modifier, ReportDistanceModifier.none);
+    expect(Visibility('6SM').qualifier, DistanceQualifier.none);
   });
 
   test('Test greater than common visibility', () {
-    expect(ReportVisibility('P6SM').modifier, ReportDistanceModifier.greater);
+    expect(Visibility('P6SM').qualifier, DistanceQualifier.more);
   });
 
   test('Test less than common fraction visibility', () {
-    expect(ReportVisibility('M1/2SM').modifier, ReportDistanceModifier.less);
+    expect(Visibility('M1/2SM').qualifier, DistanceQualifier.less);
   });
 
   test('Test common fraction visibility', () {
-    expect(ReportVisibility('1/2SM').modifier, ReportDistanceModifier.none);
+    expect(Visibility('1/2SM').qualifier, DistanceQualifier.none);
   });
 
   test('Test greater common fraction grand visibility', () {
-    expect(ReportVisibility('P1/2SM').modifier, ReportDistanceModifier.greater);
+    expect(Visibility('P1/2SM').qualifier, DistanceQualifier.more);
   });
 
   test('Test less than small fraction visibility', () {
-    expect(ReportVisibility('M1/16SM').modifier, ReportDistanceModifier.less);
+    expect(Visibility('M1/16SM').qualifier, DistanceQualifier.less);
   });
 
   test('Test small fraction visibility', () {
-    expect(ReportVisibility('1/16SM').modifier, ReportDistanceModifier.none);
+    expect(Visibility('1/16SM').qualifier, DistanceQualifier.none);
   });
 
   test('Test greater than small fraction visibility', () {
-    expect(
-        ReportVisibility('P1/16SM').modifier, ReportDistanceModifier.greater);
+    expect(Visibility('P1/16SM').qualifier, DistanceQualifier.more);
   });
 
   test('Test less than mixed fraction visibility', () {
-    expect(ReportVisibility('M2 3/4SM').modifier, ReportDistanceModifier.less);
+    expect(Visibility('M2 3/4SM').qualifier, DistanceQualifier.less);
   });
 
   test('Test mixed fraction visibility', () {
-    expect(ReportVisibility('2 3/4SM').modifier, ReportDistanceModifier.none);
+    expect(Visibility('2 3/4SM').qualifier, DistanceQualifier.none);
   });
 
   test('Test greater than mixed fraction visibility', () {
-    expect(
-        ReportVisibility('P2 3/4SM').modifier, ReportDistanceModifier.greater);
+    expect(Visibility('P2 3/4SM').qualifier, DistanceQualifier.more);
   });
 }
 
-void reportVisibilityEqualityOperator() {
-  test('Test equality operator for non-equality, no modifier', () {
-    expect(ReportVisibility('6SM') == ReportVisibility('7SM'), false);
-  });
-
-  test('Test equality operator for non-equality, with modifier', () {
-    expect(ReportVisibility('6SM') == ReportVisibility('P6SM'), false);
-  });
-
-  test('Test equality operator for equality, no modifier', () {
-    expect(ReportVisibility('6SM') == ReportVisibility('6SM'), true);
-  });
-
-  test('Test equality operator for equality, with modifier', () {
-    expect(ReportVisibility('M6SM') == ReportVisibility('M6SM'), true);
-  });
-}
-
-void reportVisibilityHashCode() {
-  test('Test hash generation for non-equality, no modifier', () {
-    expect(ReportVisibility('6SM').hashCode == ReportVisibility('7SM').hashCode,
-        false);
-  });
-
-  test('Test hash generation for non-equality, with modifier', () {
-    expect(
-        ReportVisibility('6SM').hashCode == ReportVisibility('P6SM').hashCode,
-        false);
-  });
-
-  test('Test hash generation for equality, no modifier', () {
-    expect(ReportVisibility('6SM').hashCode == ReportVisibility('6SM').hashCode,
-        true);
-  });
-
-  test('Test hash generation for equality, with modifier', () {
-    expect(
-        ReportVisibility('M6SM').hashCode == ReportVisibility('M6SM').hashCode,
-        true);
-  });
-}
-
-void reportVisibilityToString() {
+void visibilityToString() {
   test('Test less than grand visibility', () {
-    expect(ReportVisibility('M30000SM').toString(), 'M30000SM');
+    expect(Visibility('M30000SM').toString(), 'M30000SM');
   });
 
   test('Test grand visibility', () {
-    expect(ReportVisibility('30000SM').toString(), '30000SM');
+    expect(Visibility('30000SM').toString(), '30000SM');
   });
 
   test('Test greater than grand visibility', () {
-    expect(ReportVisibility('P30000SM').toString(), 'P30000SM');
+    expect(Visibility('P30000SM').toString(), 'P30000SM');
   });
 
   test('Test less than common visibility', () {
-    expect(ReportVisibility('M6SM').toString(), 'M6SM');
+    expect(Visibility('M6SM').toString(), 'M6SM');
   });
 
   test('Test common visibility', () {
-    expect(ReportVisibility('6SM').toString(), '6SM');
+    expect(Visibility('6SM').toString(), '6SM');
   });
 
   test('Test zero visibility', () {
-    expect(ReportVisibility('0SM').toString(), '0SM');
+    expect(Visibility('0SM').toString(), '0SM');
   });
 
   test('Test greater than common visibility', () {
-    expect(ReportVisibility('P6SM').toString(), 'P6SM');
+    expect(Visibility('P6SM').toString(), 'P6SM');
   });
 
   test('Test less than common fraction visibility', () {
-    expect(ReportVisibility('M1/2SM').toString(), 'M1/2SM');
+    expect(Visibility('M1/2SM').toString(), 'M1/2SM');
   });
 
   test('Test common fraction visibility', () {
-    expect(ReportVisibility('1/2SM').toString(), '1/2SM');
+    expect(Visibility('1/2SM').toString(), '1/2SM');
   });
 
   test('Test greater common fraction grand visibility', () {
-    expect(ReportVisibility('P1/2SM').toString(), 'P1/2SM');
+    expect(Visibility('P1/2SM').toString(), 'P1/2SM');
   });
 
   test('Test less than small fraction visibility', () {
-    expect(ReportVisibility('M1/16SM').toString(), 'M1/16SM');
+    expect(Visibility('M1/16SM').toString(), 'M1/16SM');
   });
 
   test('Test small fraction visibility', () {
-    expect(ReportVisibility('1/16SM').toString(), '1/16SM');
+    expect(Visibility('1/16SM').toString(), '1/16SM');
   });
 
   test('Test greater than small fraction visibility', () {
-    expect(ReportVisibility('P1/16SM').toString(), 'P1/16SM');
+    expect(Visibility('P1/16SM').toString(), 'P1/16SM');
   });
 
   test('Test less than mixed fraction visibility', () {
-    expect(ReportVisibility('M2 3/4SM').toString(), 'M2 3/4SM');
+    expect(Visibility('M2 3/4SM').toString(), 'M2 3/4SM');
   });
 
   test('Test mixed fraction visibility', () {
-    expect(ReportVisibility('2 3/4SM').toString(), '2 3/4SM');
+    expect(Visibility('2 3/4SM').toString(), '2 3/4SM');
   });
 
   test('Test greater than mixed fraction visibility', () {
-    expect(ReportVisibility('P2 3/4SM').toString(), 'P2 3/4SM');
+    expect(Visibility('P2 3/4SM').toString(), 'P2 3/4SM');
+  });
+}
+
+void visibilityEqualityOperator() {
+  test('Test equality operator for non-equality, no qualifier', () {
+    expect(Visibility('6SM') == Visibility('7SM'), false);
+  });
+
+  test('Test equality operator for non-equality, with qualifier', () {
+    expect(Visibility('6SM') == Visibility('P6SM'), false);
+  });
+
+  test('Test equality operator for equality, no qualifier', () {
+    expect(Visibility('6SM') == Visibility('6SM'), true);
+  });
+
+  test('Test equality operator for equality, with qualifier', () {
+    expect(Visibility('M6SM') == Visibility('M6SM'), true);
+  });
+}
+
+void visibilityHashCode() {
+  test('Test hash generation for non-equality, no qualifier', () {
+    expect(Visibility('6SM').hashCode == Visibility('7SM').hashCode, false);
+  });
+
+  test('Test hash generation for non-equality, with qualifier', () {
+    expect(Visibility('6SM').hashCode == Visibility('P6SM').hashCode, false);
+  });
+
+  test('Test hash generation for equality, no qualifier', () {
+    expect(Visibility('6SM').hashCode == Visibility('6SM').hashCode, true);
+  });
+
+  test('Test hash generation for equality, with qualifier', () {
+    expect(Visibility('M6SM').hashCode == Visibility('M6SM').hashCode, true);
   });
 }
